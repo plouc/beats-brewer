470bc07f990569c0522cc63b83bfd2f7faad120d
plouc <benitteraphael@gmail.com>
2021-08-02 05:46:13 +0900

    feat(demo): change demo settings

--- src/useApp.ts
+++ src/useApp.ts
@@ -99,7 +99,7 @@ const computeChannels = (
 }
 
 export const useAppStore = create<AppStore>((set, get) => ({
-    skin: skins.find((skin) => skin.id === 'flat:red')!,
+    skin: skins.find((skin) => skin.id === 'racks:black')!,
     setSkin: (skin: Skin) => set({ skin }),
     bpm: DEFAULT_BPM,
     setBpm: (bpm: number) => {
